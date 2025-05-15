60bc6239eff76d538026eb53a9352915fd213548
hannesi <hannesi@iki.fi>
2025-05-15 11:21:08 +0300

    work work

--- internal/virtualsocket/socket.go
+++ internal/virtualsocket/socket.go
@@ -6,7 +6,7 @@ import (
 	"net"
 	"time"
 
-	"github.com/hannesi/udp-rdt-go/internal/config"
+	"github.com/hannesi/go-back-n/internal/config"
 )
 
 // VirtualSocket wraps an UDP connection and simulates an unreliable network, introducing delay and bit errors before passing it to the actual UDP socket, or dropping the packet.
